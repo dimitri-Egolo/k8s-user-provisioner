fba592190c2f6acd52ea5e5e4aa90c66b71aa079
Dimitri Ntsoli <dimitri.ntsoli@users-mbp.home>
2024-11-28 13:13:48 -0500

    instrumenting python app

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 echo "The app is starting ..."
 
-opentelemetry-instrument python run.py
+opentelemetry-instrument \
+    --traces_exporter console,otlp \
+    --metrics_exporter console \
+    --service_name k8s-user-provisioner-dimitri \
+    --exporter_otlp_endpoint http://otelcol-opentelemetry-collector.zerofiltre-bootcamp.svc.cluster.local:4317 \
+    python run.py
